6a85e98e7d8fd426618227337f7c7b874ee7ac06
ahevia <alberto.hevia@seresco.es>
2014-07-24 23:57:45 +0200

    Correction of help

--- ui.R
+++ ui.R
@@ -5,6 +5,12 @@ shinyUI(pageWithSidebar(
           sidebarPanel(
           numericInput("Lat","Latitude:",34.05, min = 0, max = 70),
           numericInput("Lon","Longitude:", 118.24, min = 0, max = 160),
+          
+          helpText("Latitude and longitude (above) indicates the position of the location in the map.",
+                   "Only those events closer than the Radius in kilometeres (below) from the location will be taken into account"),
+          
+          sliderInput("Rad","Radius (in kilometers):", value = 500, min=1, max = 5000, step = 10),
+          
           dateInput("fdate","From date:", 
                     value = as.Date("2000-01-01","%Y-%m-%d"), 
                     min = as.Date("2000-01-01","%Y-%m-%d"),
@@ -17,9 +23,9 @@ shinyUI(pageWithSidebar(
                     max = as.Date("2000-12-30","%Y-%m-%d"),
                     format = "dd-mm-yyyy"
                     ),
-          sliderInput("Rad","Radius (in kilometers):", value = 500, min=1, max = 5000, step = 10),
+          helpText("These dates are used to restrict the period of time you want to evaluate.",
+                   "By default the period is between 01/01/2000 and 31/12/2010"),
           p(". "),
-          p("   "),
           p("Data obtained from Atmospheric Administration (NOAA)."),
           p("More information in: http://www.nws.noaa.gov")
           ),
